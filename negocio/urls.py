ab1d174cfcc9002ef963a5bda9d01b346551cc12
Hernansala08 <hsalazar9760@utm.edu.ec>
2024-06-08 21:18:53 +0200

    Cambiando valores

--- negocio/urls.py
+++ negocio/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from core.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index)
+    path('/', index),
+    path('admin/', admin.site.urls)
 ]
